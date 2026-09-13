3421f8ef293d57ca211a575f0ff1ccf06e7b9724
notoddish <reyeszachezekiel@gmail.com>
2026-09-13 23:28:45 +0800

    fixed health going over max health

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var enemy:= %"enemy"
 var health:= 120
 var max_health:= 120
-var damage:= 25
+var damage:= 20
 
 
 func take_damage(amount):
@@ -12,10 +12,10 @@ func take_damage(amount):
 
 func heal():
 	if battlemanager.dead != true:
-		if health < 120 and battlemanager.turn == "player":
-			health += 10
+		if health+25 < max_health and battlemanager.turn == "player":
+			health += 25
 			print("Player health: ", health)
-		elif health == 120:
+		elif health+25 >= max_health:
 			print("Cannot heal, player health: ", health)
 		elif battlemanager.turn != "player":
 			print("Cannot heal, not your turn.")
