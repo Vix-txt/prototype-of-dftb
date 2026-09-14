0a451cf0fd303157fbca6976283ebf06040bbc11
notoddish <reyeszachezekiel@gmail.com>
2026-09-14 14:00:28 +0800

    fixed heals

--- scenes/heal_button.gd
+++ scenes/heal_button.gd
@@ -1,18 +1,13 @@
 extends Button
 
 @onready var player = $"../../../player"
-var times_pressed:= 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-func check_heal_press():
-	if times_pressed > 5:
-		print("Cannot heal anymore.")
 
 func _on_pressed():
-	if times_pressed <= 5:	
+	player.check_heal_press()
+	if player.healed < 5:	
 		player.heal()
-		times_pressed += 1
-		check_heal_press()
-	
+		
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,9 +1,11 @@
 extends Node2D
 @onready var battlemanager:= $"../BattleManager"
 @onready var enemy:= %"enemy"
+@onready var healbttn:= $"../BattleUI/Control/HealButton"
 var health:= 120
 var max_health:= 120
 var damage:= 20
+var healed:= 0
 
 
 func take_damage(amount):
@@ -12,13 +14,23 @@ func take_damage(amount):
 
 func heal():
 	if battlemanager.dead != true:
-		if health+25 < max_health and battlemanager.turn == "player":
+		if health + 25 < max_health and battlemanager.turn == "player":
 			health += 25
-			print("Player health: ", health)
-		elif health+25 >= max_health:
-			print("Cannot heal, player health: ", health)
+			print("Player health : ", health)
+			healed += 1
+		if health + 25 > max_health and battlemanager.turn == "player":
+			health = max_health
+			print("Player is at max health! : ", health)
+			healed += 1
 		elif battlemanager.turn != "player":
 			print("Cannot heal, not your turn.")
 		
+		battlemanager.turn = "enemy"
+		battlemanager.enemyattack()
+		
+
+func check_heal_press():
+	if healed >= 5:
+		print("Cannot heal anymore, used all heals.")
 		battlemanager.turn = "enemy"
 		battlemanager.enemyattack()
